d814336f4591d4621e0986b52b643a76148e709c
ma16box <ma16box@gmail.com>
2026-07-04 12:58:30 +0900

    add-storage

--- infra/providers.tf
+++ infra/providers.tf
@@ -7,6 +7,12 @@ terraform {
       version = "~> 3.116"
     }
   }
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform-state"
+    storage_account_name = "storageterrastate123"
+    container_name       = "tfstate"
+    key                  = "sample.tfstate"
+  }
 }
 
 provider "azurerm" {
